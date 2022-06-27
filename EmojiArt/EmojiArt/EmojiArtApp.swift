d08ab37e35be9f0f039a128c900dddfc0cc06fca
Tan Tran <tantt.dev@gmail.com>
2022-06-27 15:02:12 -0400

    Initialize project

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Tan Tan on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
