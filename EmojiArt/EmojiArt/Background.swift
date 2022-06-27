189bb3ff3d2f4b939b333005e0f77b9ebdfd4c0d
Tan Tran <tantt.dev@gmail.com>
2022-06-27 15:25:23 -0400

    Extract `Background` enum to its own file

---
+++ EmojiArt/EmojiArt/Background.swift
@@ -0,0 +1,34 @@
+//
+//  Background.swift
+//  EmojiArt
+//
+//  Created by Tan Tan on 6/27/22.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background {
+        case bank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let uRL):
+                return uRL
+            default:
+                return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data):
+                return data
+            default:
+                return nil
+            }
+        }
+    }
+}
