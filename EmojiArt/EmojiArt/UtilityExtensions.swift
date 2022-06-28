91b05e80fb160d2aa2c6586d09ee3769d817cb4b
Tan Tran <tantt.dev@gmail.com>
2022-06-28 11:30:40 -0400

    Add extension class

---
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -0,0 +1,43 @@
+//
+//  UtilityExtensions.swift
+//  EmojiArt
+//
+//  Created by Tan Tan on 6/28/22.
+//
+
+import Foundation
+import UIKit
+
+extension Collection where Element: Identifiable {
+    func index(matching element: Element) -> Self.Index? {
+        return self.firstIndex(where: { $0.id == element.id })
+    }
+}
+
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+}
+
+extension CGPoint {
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.x - rhs.x, height: lhs.y - rhs.y)
+    }
+    
+    static func +(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
+    }
+    
+    static func -(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+    }
+    
+    static func *(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
+    static func /(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
