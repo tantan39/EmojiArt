61499c47d30b1dd8310a9dd357a130a00cdb8871
Tan Tran <tantt.dev@gmail.com>
2022-06-28 21:21:06 -0400

    Update `loadObjects` helper methods to load `NSItemProvider`

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -14,6 +14,16 @@ extension Collection where Element: Identifiable {
     }
 }
 
+extension Character {
+    var isEmoji: Bool {
+        if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
+            return (firstScalar.value >= 0x238d || unicodeScalars.count > 1)
+        } else {
+            return false
+        }
+    }
+}
+
 extension CGRect {
     var center: CGPoint {
         CGPoint(x: midX, y: midY)
@@ -39,5 +49,35 @@ extension CGPoint {
     
     static func /(lhs: Self, rhs: CGFloat) -> CGPoint {
         CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
+
+extension Array where Element == NSItemProvider {
+    func loadObjects<T>(offType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType)}) {
+            provider.loadObject(ofClass: theType) { object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadObjects<T>(offType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType)}) {
+            let _ = provider.loadObject(ofClass: theType) { object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
     }
 }
