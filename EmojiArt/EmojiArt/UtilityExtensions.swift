fb06fb005582611153cb5e4a85a0ebf1b1a673cf
Tan Tran <tantt.dev@gmail.com>
2022-07-05 22:10:49 -0400

    Update extension

--- EmojiArt/EmojiArt/UtilityExtensions.swift
+++ EmojiArt/EmojiArt/UtilityExtensions.swift
@@ -66,6 +66,28 @@ extension CGPoint {
     }
 }
 
+extension CGSize {
+    var center: CGPoint {
+        CGPoint(x: width/2, y: height/2)
+    }
+    
+    static func +(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+    
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+    }
+    
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+    }
+}
+
 extension Array where Element == NSItemProvider {
     func loadObjects<T>(offType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
         if let provider = first(where: { $0.canLoadObject(ofClass: theType)}) {
