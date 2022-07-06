3a3ef4a0d7a81a626e28ff203105afa8609c06d0
Tan Tran <tantt.dev@gmail.com>
2022-07-06 12:41:25 -0400

    Implement `Codable` logic on enum associate type

--- EmojiArt/EmojiArt/Background.swift
+++ EmojiArt/EmojiArt/Background.swift
@@ -8,10 +8,38 @@
 import Foundation
 
 extension EmojiArtModel {
-    enum Background: Equatable {
+    enum Background: Equatable, Codable {
         case bank
         case url(URL)
         case imageData(Data)
+        
+        enum CodingKeys: String, CodingKey {
+            case url = "theURL"
+            case imageData
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            if let url = try? container.decode(URL.self, forKey: .url) {
+                self = .url(url)
+            } else if let imageData = try? container.decode(Data.self, forKey: .imageData) {
+                self = .imageData(imageData)
+            } else {
+                self = .bank
+            }
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case .bank:
+                break
+            case .url(let uRL):
+                try container.encode(uRL, forKey: .url)
+            case .imageData(let data):
+                try container.encode(data, forKey: .imageData)
+            }
+        }
         
         var url: URL? {
             switch self {
